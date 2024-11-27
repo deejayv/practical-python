40910c11d7d1eed411ebfdf4375de2cd1a037e82
Adedeji ADEGBENRO <adegbenroa@CEPC.local>
2024-11-27 16:33:39 -0500

    Completed exercise 1.33

--- Work/pcost.py
+++ Work/pcost.py
@@ -23,18 +23,145 @@
 # portfolio_open.close()
 
 
-# Solution
+# # Solution
 
-total_cost = 0.0
+# total_cost = 0.0
 
-with open('Data/portfolio.csv', 'rt') as f:
-    headers = next(f)
+# with open('Data/portfolio.csv', 'rt') as f:
+    # headers = next(f)
     
-    for line in f:
-        row = line.split(',')
-        nshares = int(row[1])
-        price = float(row[2])
-        total_cost += nshares * price
+    # for line in f:
+        # row = line.split(',')
+        # nshares = int(row[1])
+        # price = float(row[2])
+        # total_cost += nshares * price
         
-print('Total cost', total_cost)
+# print('Total cost', total_cost)
 
+# # pcost.py
+# #
+# # Exercise 1.30 (Turning a script into a function)
+
+# def portfolio_cost(filename):
+    
+    # 'This function calculates the total cost of the portfolio in the file'
+    
+    # total_cost = 0.0
+    
+    # with open(filename, 'rt') as f:
+        
+        # headers = next(f)
+        
+        # for line in f:
+            # row = line.split(',')
+            # nshares = int(row[1])
+            # price = float(row[2])
+            # total_cost += nshares * price
+            
+    # return total_cost
+    
+# cost = portfolio_cost('Data/portfolio.csv')
+# print('Total cost:', cost)
+
+# # pcost.py
+# #
+# # Exercise 1.31: Errorhandling (Modify the pcost.py program to catch the exception, print a warning message, and continue processing the rest of the file)
+
+# def portfolio_cost(filename):
+    
+    # 'This function calculates the total cost of the portfolio in the file'
+    
+    # total_cost = 0.0
+    
+    # with open(filename, 'rt') as f:
+        
+        # headers = next(f)
+        
+        # for line in f:
+            # row = line.split(',')
+            
+            # try:
+                # nshares = int(row[1])
+                # price = float(row[2])
+                # total_cost += nshares * price
+            # except ValueError:
+                # print("Couldn't parse", line)
+            
+    # return total_cost
+    
+# cost = portfolio_cost('Data/portfolio.csv')
+# print('Total cost:', cost)
+
+# # pcost.py
+# #
+# # Exercise 1.32: Using a library function (Modify the program so that it uses the csv module for parsing and try running earlier examples)
+
+# import csv
+
+# def portfolio_cost(filename):
+    
+    # 'This function calculates the total cost of the portfolio in the file'
+    
+    # total_cost = 0.0
+    
+    # with open(filename, 'rt') as f:
+        
+        # rows = csv.reader(f)
+        
+        # headers = next(rows)
+        
+        # # print(headers)
+        
+        # for row in rows:
+            
+            # try:
+                # nshares = int(row[1])
+                # price = float(row[2])
+                # total_cost += nshares * price
+            # except ValueError:
+                # print("Couldn't parse", row)
+            
+    # return total_cost
+    
+# cost = portfolio_cost('Data/portfolio.csv')
+# print('Total cost:', cost)
+
+# pcost.py
+#
+# Exercise 1.33: Reading from the command line (Pass the name of the file in as an argument, in the terminal, to a script)
+
+import csv
+import sys
+
+def portfolio_cost(filename):
+    
+    'This function calculates the total cost of the portfolio in the file'
+    
+    total_cost = 0.0
+    
+    with open(filename, 'rt') as f:
+        
+        rows = csv.reader(f)
+        
+        headers = next(rows)
+        
+        # print(headers)
+        
+        for row in rows:
+            
+            try:
+                nshares = int(row[1])
+                price = float(row[2])
+                total_cost += nshares * price
+            except ValueError:
+                print("Couldn't parse", row)
+            
+    return total_cost
+   
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+    
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
