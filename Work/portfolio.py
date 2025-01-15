d5090a509cd5f18a2d58836c73a56ec00f36dcee
Adedeji ADEGBENRO <adegbenroa@CEPC.local>
2025-01-15 17:06:07 -0500

    Completed till Exercise 6.15

--- Work/portfolio.py
+++ Work/portfolio.py
@@ -21,7 +21,37 @@
             # total_shares[s.name] += s.shares
         # return total_shares
 
-# Module 6.1 Exercise 6.3: Making a more proper container
+# # Module 6.1 Exercise 6.3: Making a more proper container
+
+# class Portfolio:
+    
+    # def __init__(self, holdings):
+        # self._holdings = holdings
+
+    # def __iter__(self):
+        # return self._holdings.__iter__()
+
+    # def __len__(self):
+        # return len(self._holdings)
+    
+    # def __getitem__(self, index):
+        # return self._holdings[index]
+    
+    # def __contains__(self, name):
+        # return any([s.name == name for s in self._holdings]) 
+    
+    # @property
+    # def total_cost(self):
+        # return sum([s.cost for s in self._holdings])
+
+    # def tabulate_shares(self):
+        # from collections import Counter
+        # total_shares = Counter()
+        # for s in self._holdings:
+            # total_shares[s.name] += s.shares
+        # return total_shares
+
+# Module 6.4 Exercise 6.14: Generator Expressions in Functions Arguments
 
 class Portfolio:
     
@@ -38,11 +68,13 @@ class Portfolio:
         return self._holdings[index]
     
     def __contains__(self, name):
-        return any([s.name == name for s in self._holdings]) 
+        # return any([s.name == name for s in self._holdings])
+        return any(s.name == name for s in self._holdings)
     
     @property
     def total_cost(self):
-        return sum([s.cost for s in self._holdings])
+        # return sum([s.cost for s in self._holdings])
+        return sum(s.cost for s in self._holdings)
 
     def tabulate_shares(self):
         from collections import Counter
--- Work/ticker.py
+++ Work/ticker.py
@@ -65,7 +65,57 @@
     # for row in rows:
         # print(row)
 
-# Module 6.2 Exercise 6.12: Puttng it all together
+# # Module 6.2 Exercise 6.12: Puttng it all together
+
+# import report
+# from follow import follow
+# import csv
+# import tableformat
+
+# def select_columns(rows, indices):
+    # for row in rows:
+        # yield [row[index] for index in indices]
+
+# def convert_types(rows, types):
+    # for row in rows:
+        # yield [func(val) for func, val in zip(types, row)]
+
+# def make_dicts(rows, headers):
+    # for row in rows:
+        # yield dict(zip(headers, row))
+
+# def filter_symbols(rows, names):
+    # for row in rows:
+        # if row['name'] in names:
+            # yield row
+
+# def parse_stock_data(lines):
+    # rows = csv.reader(lines)
+    # rows = select_columns(rows, [0, 1, 4])
+    # rows = convert_types(rows, [str, float, float])
+    # rows = make_dicts(rows, ['name', 'price', 'change'])
+    # return rows
+
+# def ticker(portfile, logfile, fmt):
+    # portfolio = report.read_portfolio(portfile)
+    # lines = follow(logfile)
+    # rows = parse_stock_data(lines)
+    # rows = filter_symbols(rows, portfolio)
+    # formatter = tableformat.create_formatter(fmt)
+    # formatter.headings(['Name', 'Price', 'Change'])
+    # for row in rows:
+        # formatter.row([ row['name'], f"{row['price']:0.2f}", f"{row['change']:0.2f}" ])
+
+# def main(args):
+    # if len(args) != 4:
+        # raise SystemExit('USage: %s portfoliofile logfile fmt' % args[0])
+    # ticker(args[1], args[2], args[3])
+
+# if __name__ == '__main__':
+    # import sys
+    # main(sys.argv)
+    
+# Module 6.4 Exercise 6.15: Code simplification
 
 import report
 from follow import follow
@@ -81,13 +131,14 @@ def convert_types(rows, types):
         yield [func(val) for func, val in zip(types, row)]
 
 def make_dicts(rows, headers):
-    for row in rows:
-        yield dict(zip(headers, row))
+    # for row in rows:
+        # yield dict(zip(headers, row))
+    return (dict(zip(headers, row)) for row in rows) # Generator expression
 
-def filter_symbols(rows, names):
-    for row in rows:
-        if row['name'] in names:
-            yield row
+# def filter_symbols(rows, names):
+    # for row in rows:
+        # if row['name'] in names:
+            # yield row
 
 def parse_stock_data(lines):
     rows = csv.reader(lines)
@@ -100,7 +151,8 @@ def ticker(portfile, logfile, fmt):
     portfolio = report.read_portfolio(portfile)
     lines = follow(logfile)
     rows = parse_stock_data(lines)
-    rows = filter_symbols(rows, portfolio)
+    # rows = filter_symbols(rows, portfolio)
+    rows = (row for row in rows if row['name'] in portfolio) # Generator expression
     formatter = tableformat.create_formatter(fmt)
     formatter.headings(['Name', 'Price', 'Change'])
     for row in rows:
@@ -108,7 +160,7 @@ def ticker(portfile, logfile, fmt):
 
 def main(args):
     if len(args) != 4:
-        raise SystemExit('USage: %s portfoliofile logfile fmt' % args[0])
+        raise SystemExit('Usage: %s portfoliofile logfile fmt' % args[0])
     ticker(args[1], args[2], args[3])
 
 if __name__ == '__main__':
